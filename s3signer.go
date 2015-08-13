054ab5f8f53f59b5bd1a13d7bfe27bc150f930df
a-tal <github@talsma.ca>
2015-08-13 18:39:18 +0000

    super important changes :|

--- s3signer.go
+++ s3signer.go
@@ -1,98 +1,98 @@
 package main
 
 import (
-    "io"
-    "os"
-    "log"
-    "time"
-    "errors"
-    "net/http"
-    "github.com/gorilla/mux"
-    "github.com/mitchellh/goamz/aws"
-    "github.com/mitchellh/goamz/s3"
+	"errors"
+	"github.com/gorilla/mux"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 type S3Client struct {
-    client  *s3.S3
-    buckets []s3.Bucket
+	client  *s3.S3
+	buckets []s3.Bucket
 }
 
-func s3init(region aws.Region) (S3Client) {
-    auth, err := aws.EnvAuth()
-    if err != nil {
-      log.Fatal(err)
-    }
-    client := s3.New(auth, region)
-    buckets, bucketerr := client.ListBuckets()
-    if bucketerr != nil {
-        log.Fatal(bucketerr)
-    }
+func s3init(region aws.Region) S3Client {
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := s3.New(auth, region)
+	buckets, bucketerr := client.ListBuckets()
+	if bucketerr != nil {
+		log.Fatal(bucketerr)
+	}
 
-    return S3Client{
-        client: client,
-        buckets: buckets.Buckets,
-    }
+	return S3Client{
+		client:  client,
+		buckets: buckets.Buckets,
+	}
 }
 
 func (s3 *S3Client) bucketFileHandler(w http.ResponseWriter, req *http.Request) {
-    vars := mux.Vars(req)
-    bucket := vars["bucket"]
-    filename := vars["filename"]
+	vars := mux.Vars(req)
+	bucket := vars["bucket"]
+	filename := vars["filename"]
 
-    for i := 0; i < len(s3.buckets); i++ {
-        if bucket == s3.buckets[i].Name {
+	for i := 0; i < len(s3.buckets); i++ {
+		if bucket == s3.buckets[i].Name {
 
-            keys, err := s3.buckets[i].GetBucketContents()
-            if err != nil {
-                log.Fatal("Error listing bucket: ", err)
-            }
+			keys, err := s3.buckets[i].GetBucketContents()
+			if err != nil {
+				log.Fatal("Error listing bucket: ", err)
+			}
 
-            for _, key := range *keys {
-                if filename == key.Key {
-                    expires := time.Now()
-                    expires.Add(time.Duration(5))
-                    url := s3.buckets[i].SignedURL(filename, expires)
-                    io.WriteString(w, url)
-                    return
-                }
-            }
-            return
-        }
-    }
+			for _, key := range *keys {
+				if filename == key.Key {
+					expires := time.Now()
+					expires.Add(time.Duration(5))
+					url := s3.buckets[i].SignedURL(filename, expires)
+					io.WriteString(w, url)
+					return
+				}
+			}
+			return
+		}
+	}
 }
 
 func getRegion() (region aws.Region, err error) {
-    reqRegion := os.Getenv("AWS_REGION")
-    if reqRegion == "" {
-        err = errors.New("missing AWS_REGION environment variable")
-        return
-    }
+	reqRegion := os.Getenv("AWS_REGION")
+	if reqRegion == "" {
+		err = errors.New("missing AWS_REGION environment variable")
+		return
+	}
 
-    for name, awsRegion := range aws.Regions {
-        if reqRegion == name {
-            region = awsRegion
-            return
-        }
-    }
+	for name, awsRegion := range aws.Regions {
+		if reqRegion == name {
+			region = awsRegion
+			return
+		}
+	}
 
-    err = errors.New("invalid region: " + reqRegion)
-    return
+	err = errors.New("invalid region: " + reqRegion)
+	return
 }
 
 func main() {
-    region, err := getRegion()
-    if err != nil {
-        log.Fatal(err)
-    }
+	region, err := getRegion()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    s3 := s3init(region)
+	s3 := s3init(region)
 
-    r := mux.NewRouter()
-    r.HandleFunc("/{bucket}/{filename}/", s3.bucketFileHandler)
-    http.Handle("/", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/{bucket}/{filename}/", s3.bucketFileHandler)
+	http.Handle("/", r)
 
-    httperr := http.ListenAndServe(":8080", nil)
-    if httperr != nil {
-        log.Fatal("http error: ", httperr)
-    }
+	httperr := http.ListenAndServe(":8080", nil)
+	if httperr != nil {
+		log.Fatal("http error: ", httperr)
+	}
 }
